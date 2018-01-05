eab44541fe161bcfdd36e0b3d2ef7da20d95ad33
SaiTanuja96 <batchali.saitanuja@gmail.com>
2018-01-05 16:49:43 +0530

    addedphoto slide

---
+++ index.js
@@ -0,0 +1,7 @@
+var mainText = document.getElementById("username");
+var mainText1 = document.getElementById("password");
+var submitbtn = document.getElementById("submitbtn");
+function checkinfo(){
+
+    window.alert("cool");
+}
